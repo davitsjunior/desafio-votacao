62418ace28552b754c991f2772567cbb721b0578
Davi Tomas da Silva Junior <dv_junior@hotmail.com>
2026-08-07 04:32:45 -0300

    feat: create sessao

---
+++ src/main/java/br/com/davijunior/desafio_votacao/controller/SessaoController.java
@@ -0,0 +1,29 @@
+package br.com.davijunior.desafio_votacao.controller;
+
+import br.com.davijunior.desafio_votacao.dto.request.SessaoRequest;
+import br.com.davijunior.desafio_votacao.dto.response.SessaoResponse;
+import br.com.davijunior.desafio_votacao.service.SessaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pautas/{pautaId}/sessao")
+@RequiredArgsConstructor
+public class SessaoController {
+
+    private final SessaoService sessaoService;
+
+    @PostMapping
+    public ResponseEntity<SessaoResponse> abrir(@PathVariable Long pautaId,
+                                                 @RequestBody(required = false) SessaoRequest request) {
+        SessaoRequest body = request != null ? request : new SessaoRequest(null);
+        SessaoResponse response = sessaoService.abrir(pautaId, body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
--- src/main/java/br/com/davijunior/desafio_votacao/repository/SessaoRepository.java
+++ src/main/java/br/com/davijunior/desafio_votacao/repository/SessaoRepository.java
@@ -4,4 +4,6 @@ import br.com.davijunior.desafio_votacao.entity.Sessao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SessaoRepository extends JpaRepository<Sessao, Long> {
+
+    boolean existsByPautaId(Long pautaId);
 }
